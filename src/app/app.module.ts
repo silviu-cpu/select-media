4d9e67f361cc538bd3c70c9f334acca8acd7c754
Silviu Barbu <barbu.silviu92@gmail.com>
2025-07-19 07:52:52 +0300

    Pushed new components for feature SELECT-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    HeroSectionComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
